00982fc1c0257bbddc07c79fd8408ef64ec8c503
chiang <emmet.jiang@gmail.com>
2021-03-04 22:49:49 +0800

    add form rules

--- vue-element/src/App.vue
+++ vue-element/src/App.vue
@@ -1,58 +1,20 @@
 <template>
   <div>
-    <div>
-      <el-row :gutter="20">
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6" :offset="6"
-          ><div class="grid-content bg-purple"></div
-        ></el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="6" :offset="6"
-          ><div class="grid-content bg-purple"></div
-        ></el-col>
-        <el-col :span="6" :offset="6"
-          ><div class="grid-content bg-purple"></div
-        ></el-col>
-      </el-row>
-      <div>最后一行不能是其他的元素，必须是el-row时，last-child才能有作用</div>
-      <el-row :gutter="20">
-        <el-col :span="12" :offset="6"
-          ><div class="grid-content bg-purple"></div
-        ></el-col>
-      </el-row>
-    </div>
-    <div>haha</div>
+    <hello-world></hello-world>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/form/Validate'
 
 export default {
   name: "App",
-  // components: {
-  //   HelloWorld
-  // }
+  components: {
+    HelloWorld
+  }
 };
 </script>
 
 <style lang="scss">
-.el-row {
-  background-color: blue;
-  margin-bottom: 20px;
-  &:last-child {
-    margin-bottom: 0px;
-  }
-}
-.el-col {
-  border-radius: 4px;
-}
-.bg-purple {
-  background: #d3dce6;
-}
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
-}
+
 </style>
---
+++ vue-element/src/components/css/LastChild.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div>
+      <div>el-row last-child，必须是el-row父控件里的最后一个el-row才有效，其他控件无效</div>
+      <el-row :gutter="20">
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6" :offset="6"
+          ><div class="grid-content bg-purple"></div
+        ></el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="6" :offset="6"
+          ><div class="grid-content bg-purple"></div
+        ></el-col>
+        <el-col :span="6" :offset="6"
+          ><div class="grid-content bg-purple"></div
+        ></el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="12" :offset="6"
+          ><div class="grid-content bg-purple"></div
+        ></el-col>
+      </el-row>
+    </div>
+    <div>haha</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "LastChildCSS"
+};
+</script>
+
+<style lang="scss">
+.el-row {
+  background-color: blue;
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0px;
+  }
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+</style>
